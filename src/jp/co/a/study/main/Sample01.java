abb1ca2504165695d16f365824549c397851cd08
MYCOMPUTER\airle <airlens1127@yahoo.co.jp>
2017-11-04 01:24:39 +0900

    バージョン８

--- src/jp/co/a/study/main/Sample01.java
+++ src/jp/co/a/study/main/Sample01.java
@@ -5,9 +5,6 @@ public class Sample01 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("Hello World");
-		System.out.println("BeutifulWorld!!");
-
-
 
 	}
 
